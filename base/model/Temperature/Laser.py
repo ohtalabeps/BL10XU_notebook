fcc3bfe04ac14a942c85c6253bb470d168da4f49
Osamu Ishizawa <kiaydr@gmail.com>
2024-06-15 21:45:59 +0900

    feat: 定常加熱・最後だけ定常の線形加熱に対応

--- base/model/Temperature/Laser.py
+++ base/model/Temperature/Laser.py
@@ -9,177 +9,127 @@ import numpy as np
 class LaserProfile():
     def __init__(self):
         """
-        Linear加熱の時のinputパラメータ
         (Laser linear controlの画面を参照している)
         """
-        # self.input_option = {
-        #         "start_power-W"    : 10,
-        #         "goal_power-W"     : 50,
-        #         "delay-ms"         : 1000,
-        #         "time_adjust-ms"   : 100,
-        #         "width-ms"         : 8000,
-        #         "step_width-ms"    : 100,
-        #         "residue-ms"       : 1000,
-        #         "laser_diameter-um": 17.5
-        # }
-        self.start_power = None
-        self.goal_power = None
-        self.delay = 1_000
-        self.time_adjust = 100
-        self.width = 8_000
-        self.step_width = 100
-        self.residue = 1_000
-        self.laser_diameter = 17.5
+        self.linear = False
+        self.delay_ms = 1_000
+        self.time_adjust_ms = 100
+        self.power_W = None
+        self.base_width_ms = None
+        self.whole_time_ms = None # plotの終わり時間を決める
+        self.laser_diameter_um = None
+        if self.linear: # 線形加熱の場合
+            self.start_power_W = None
+            self.goal_power_W = None
+            self.step_width_ms = None
+            self.linear_width_ms = None
 
-    def get_plot_values(self):
-        self.step_num = self.width / self.step_width # 何回エネルギーが変化するか
-        # 最後は変化した瞬間に止まる(はず)なので，-1している
-        self.energy_step = (self.goal_power - self.start_power) / self.step_num #(W), 一回あたりのエネルギー変化幅
+    def set_from_json(self, json):
+        # TODO
+        pass
 
-        self.time_series = np.arange(
-                self.delay, # start
-                self.delay + self.width + 1, #end, endは含まないため+1している。ms単位なので最後が含まれるようになるだけ
-                self.step_width # step
-                )
-        self.energy_series = np.arange(
-                self.start_power,
-                self.goal_power,
-                self.energy_step
-                )
-
-        self.step_x = (self.time_series + self.time_adjust) / 1000 #time(s), ユニバーサル演算
-        self.step_y = np.r_[np.zeros(1), self.energy_series] #Total Energy(W), 先頭に0を追加している。np.r_は1次元配列の結合
-
-        self._compute_arr() # 一括でplotできる配列を作成。↑は消すのめんどくさくて残してるだけ
-
-    def _compute_arr(self):
-        """時間配列"""
-        # start
-        time_arr = np.zeros(1)
-        # 加熱はじめ
-        time_arr = np.r_[time_arr, self.delay + self.time_adjust]
-        time_arr = np.r_[time_arr, self.delay + self.time_adjust]
-        # step加熱
-        # FIXME
-        #   time_adjustは一回にする
-        #   time_series[i+1]がわかりづらい。計算する
-        for i in range(int(self.step_num)):
-            time_arr = np.r_[time_arr, self.time_series[i + 1] + self.time_adjust]
-            time_arr = np.r_[time_arr, self.time_series[i + 1] + self.time_adjust]
-        # 加熱終わり
-        time_arr = np.r_[time_arr, time_arr[-1]]  # クエンチ
-        end_time = self.delay + self.width + self.residue
-        time_arr = np.r_[time_arr, end_time]
-        time_arr /= 1000
-        self.time_arr = time_arr
-
-        """エネルギー配列"""
-        # start
-        energy_arr = np.zeros(1)
-        # 加熱はじめ
-        energy_arr = np.r_[energy_arr, np.array([0])]
-        energy_arr = np.r_[energy_arr, self.start_power]
-        # step加熱
-        energy = self.start_power
-        for i in range(int(self.step_num)):
-            energy_arr = np.r_[energy_arr, energy]
-            energy += self.energy_step
-            energy_arr = np.r_[energy_arr, energy]
-        # 加熱終わり
-        energy_arr = np.r_[energy_arr, np.zeros(1)]
-        energy_arr = np.r_[energy_arr, np.zeros(1)]
-        self.energy_arr = energy_arr
-
-    def show_profile_plot(self, grid=True) -> None:
-        figsize = (4, 4)
-        dpi = 1000
-        facecolor = 'white'
-        
-        fig = plt.figure(figsize=figsize, dpi=dpi, facecolor=facecolor)
-        ax = fig.add_subplot(1, 1, 1)
-        
-        color = (1,0,0,1)# 'red'
-        linewidth = 1
-        # TODO delay + timeadjustをx1みたいな変数にする。説明で図示を使えるし，ax.plotの可読性も上がる
-        # 1. line1
-        ax.plot(
-                [0, (self.delay + self.time_adjust) / 1000],
-                [0, 0],
-                color = color,
-                linewidth=linewidth
-                )
-        # 2. step
-        ax.step(
-                self.step_x,
-                self.step_y,
-                where = 'pre',
-                color = color,
-                # color = 'blue' # どこがstepで書かれているか知りたい時はこっちをコメントアウト
-                linewidth=linewidth
-                )
-        # 3. line2
-        ax.plot(
-                [(self.delay + self.time_adjust + self.width) / 1000, (self.delay + self.time_adjust + self.width) / 1000],
-                [self.goal_power - self.energy_step, 0],
-                color = color,
-                linewidth=linewidth
-                )
-        # 4. line3
-        ax.plot(
-                [(self.delay + self.time_adjust + self.width) / 1000, (self.delay + self.width + self.residue) / 1000],
-                [0, 0],
-                color = color,
-                linewidth=linewidth
-                )
-        ax.set_title('Laser Profile')
-        ax.set_xlabel('Time (s)')
-        ax.set_ylabel('Total Power (W)')
-        if grid:
-            ax.grid()
+    def get_laser_profile_arr(self):
+        if self.linear: # 線形加熱の場合
+            print(" -- linear 加熱 --")
+        else: # 定常加熱の場合
+            print(" -- burst 加熱 --")
+        self._compute_time_arr()
+        self._compute_power_arr()
 
 
-    def overlap_laser_profile(self, ax) -> plt.axes:
-        # 別のy軸で重ねる
-        twin = ax.twinx()
-        color = (1,0,0,0.5)
-        linewidth = 1
-        # TODO delay + timeadjustをx1みたいな変数にする。説明で図示を使えるし，ax.plotの可読性も上がる
+    def _compute_time_arr(self):
+        # 線形加熱の場合
+        if self.linear:
+            if self.linear_width_ms == self.base_width_ms:
+                step_num = round(self.linear_width_ms / self.step_width_ms)
+                time_list = [
+                    0,
+                    self.delay_ms + self.time_adjust_ms,
+                    self.delay_ms + self.time_adjust_ms
+                ]
+                for i in range(step_num):
+                    ele = time_list[-1] + self.step_width_ms # 時間を進める
+                    time_list.append(ele) # 追加
+                    time_list.append(ele) # レーザー出力のみ上がる
+                time_list.append(time_list[-1]) # クエンチ
+                time_list.append(self.whole_time_ms) # 最後
+            elif self.linear_width_ms < self.base_width_ms:
+                step_num = round(self.linear_width_ms / self.step_width_ms)
+                time_list = [
+                    0,
+                    self.delay_ms + self.time_adjust_ms,
+                    self.delay_ms + self.time_adjust_ms
+                ]
+                for i in range(step_num):
+                    ele = time_list[-1] + self.step_width_ms # 時間を進める
+                    time_list.append(ele) # 追加
+                    time_list.append(ele) # レーザー出力のみ上がる
+                time_list.append(self.base_width_ms) # ここまで保つ
+                time_list.append(time_list[-1]) # クエンチ
+                time_list.append(self.whole_time_ms) # 最後
+            else:
+                raise Exception("線形加熱のwidthが長い場合は実装されていません。実装してください。")
+        # 定常加熱の場合
+        else:
+            time_list = [
+                0, # 1点目
+                self.delay_ms + self.time_adjust_ms,
+                self.delay_ms + self.time_adjust_ms,
+                self.delay_ms + self.time_adjust_ms + self.base_width_ms,
+                self.delay_ms + self.time_adjust_ms + self.base_width_ms,
+                self.whole_time_ms # 最後
+            ]
+        time_arr = np.array(time_list)
+        self.time_arr = time_arr / 1_000 # ms -> s
+        print("\t時間計算終わり")
 
-        # 1. line1
-        twin.plot(
-                [0, (self.delay + self.time_adjust) / 1000],
-                [0, 0],
-                color = color,
-                linewidth=linewidth
-                )
-        # 2. step
-        twin.step(
-                self.step_x,
-                self.step_y,
-                where = 'pre',
-                color = color,
-                linewidth=linewidth,
-                label = 'Laser Power'
-                )
-        # 3. line2
-        twin.plot(
-                [(self.delay + self.time_adjust + self.width) / 1000, (self.delay + self.time_adjust + self.width) / 1000],
-                [self.goal_power - self.energy_step, 0],
-                color = color,
-                linewidth=linewidth,
-                )
-        # 4. line3
-        twin.plot(
-                [(self.delay + self.time_adjust + self.width) / 1000, (self.delay + self.width + self.residue) / 1000],
-                [0, 0],
-                color = color,
-                linewidth=linewidth,
-                )
-        twin.legend()
-
-        twin.set_ylabel('Total Power (W)')
-        return ax
-
+    def _compute_power_arr(self):
+        # 線形加熱の場合
+        if self.linear:
+            if self.linear_width_ms == self.base_width_ms:
+                step_num = round(self.linear_width_ms / self.step_width_ms)
+                power_step = (self.goal_power_W - self.start_power_W) / step_num
+                power_list = [
+                    0,
+                    0,
+                    self.start_power_W
+                ]
+                for i in range(step_num):
+                    ele = power_list[-1]
+                    power_list.append(ele) # 時間のみ進む
+                    power_list.append(ele + power_step) # エネルギー上がる
+                power_list.append(0) # クエンチ
+                power_list.append(0) # 最後
+            elif self.linear_width_ms < self.base_width_ms:
+                step_num = round(self.linear_width_ms / self.step_width_ms)
+                power_step = (self.goal_power_W - self.start_power_W) / step_num
+                power_list = [
+                    0,
+                    0,
+                    self.start_power_W
+                ]
+                for i in range(step_num):
+                    ele = power_list[-1]
+                    power_list.append(ele) # 時間のみ進む
+                    power_list.append(ele + power_step) # エネルギー上がる
+                power_list.append(power_list[-1]) # 最後のエネルギーを保つ
+                power_list.append(0) # クエンチ
+                power_list.append(0) # 最後
+            else:
+                raise Exception("線形加熱のwidthが長い場合は実装されていません。実装してください。")
+        # 定常加熱の場合
+        else:
+            power_list = [
+                0, # 1点目
+                0,
+                self.power_W,
+                self.power_W,
+                0,
+                0 # 最後
+            ]
+        power_arr = np.array(power_list)
+        self.power_arr = power_arr
+        print("\tエネルギー計算終わり")
 
 
 if __name__ == "__main__":
